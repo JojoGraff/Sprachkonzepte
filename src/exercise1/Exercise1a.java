c196cf1640ce7b6a7a79f0d739b8cab7250bedc3
JojoGraff <johannes.graff2001@gmail.com>
2023-02-13 21:38:41 +0100

    updated

--- src/exercise1/Exercise1a.java
+++ src/exercise1/Exercise1a.java
@@ -11,6 +11,7 @@ public final class Exercise1a {
         String test1 = "xxx%1$d yyy";
         String test2 = "%1$-02.3dyyy";
         String test3 = "Wochentag: %tA Uhrzeit: %tT";
+        String[] test = {test0, test1, test2, test3};
 
 
         String text = "([a-zA-Z: ]*)";
@@ -19,15 +20,32 @@ public final class Exercise1a {
         String index = "([1-9][0-9]*\\$)*";
         String width = "(\\d*)?";
         String precision = "(\\.\\d*)?";
+        String time = "[AT]?";
 
-        Pattern regex = Pattern.compile("(%" + index + flags + width + precision + conversions + ")|" + text);
-        Matcher matcher = regex.matcher(test3);
+        Pattern regex = Pattern.compile("(%" + index + flags + width + precision + conversions + time + ")|" + text);
 
-        while (matcher.find() && !matcher.group().equals("")) {
-            if (matcher.group().startsWith("%"))
-                System.out.printf("FORMAT(%s)", matcher.group());
-            else
-                System.out.printf("TEXT(%s)", matcher.group());
+
+        for (String teststring : test) {
+            boolean prev = false;
+            Matcher matcher = regex.matcher(teststring);
+            while (matcher.find() && !matcher.group().equals("")) {
+                if (matcher.group().startsWith("%"))
+                    System.out.printf("FORMAT(\"%s\")", matcher.group());
+                else {
+                    if (matcher.group().startsWith(" ") && matcher.group().endsWith(": ") && prev) {
+                        System.out.printf("TEXT(\"%s\")", matcher.group().substring(1, matcher.group().length() - 1));
+                    } else if (matcher.group().startsWith(" ") && prev) {
+                        System.out.printf("TEXT(\"%s\")", matcher.group().substring(1));
+                    } else if (matcher.group().endsWith(": ")) {
+                        System.out.printf("TEXT(\"%s\")", matcher.group().substring(0, matcher.group().length() - 1));
+                    } else {
+                        System.out.printf("TEXT(\"%s\")", matcher.group());
+                    }
+                }
+                prev = matcher.group().contains("%t");
+            }
+            System.out.println("");
         }
     }
 }
+
